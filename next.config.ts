8346088dd0e592d7b106c75883c8dd3c5470433f
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-06-25 21:33:45 +0300

    add: cors Config for migratting React admin

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     useCache: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:5173",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
